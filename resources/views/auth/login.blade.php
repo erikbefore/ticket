8c778bdfecea0a1e3c5447bb31f1b7d9aea50d00
Erik Lopes <erik@before.com.br>
2018-12-28 18:57:26 -0300

    Ajustes e correções

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,27 +39,27 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        {{--<div class="form-group row" sytle="display:none">--}}
+                            {{--<div class="col-md-6 offset-md-4">--}}
+                                {{--<div class="form-check">--}}
+                                    {{--<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                                    {{--<label class="form-check-label" for="remember">--}}
+                                        {{--{{ __('Lembrar-me') }}--}}
+                                    {{--</label>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Entrar') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Esqueci minha senha') }}
                                     </a>
                                 @endif
                             </div>
